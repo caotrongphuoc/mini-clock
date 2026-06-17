25e6720757685fb66eca000a2bf2f2f9799b4526
Cao Trong Phuoc <caotrongphuoc@gmail.com>
2026-06-17 11:53:29 +0700

    [UPDATE] remove redundant gunner_y static and modify gunner.y directly

--- application/sources/app/game/game_zomwar/zw_game_gunner.cpp
+++ application/sources/app/game/game_zomwar/zw_game_gunner.cpp
@@ -1,7 +1,6 @@
 #include "zw_game_gunner.h"
 
 zw_game_gunner_t gunner;
-static uint8_t gunner_y;
 
 void zw_game_gunner_handle(ak_msg_t* msg)
 {
@@ -15,34 +14,32 @@ void zw_game_gunner_handle(ak_msg_t* msg)
 		gunner.y = GUNNER_AXIS_Y;
 		gunner.visible = WHITE;
 		gunner.action_image = 1;
-		gunner_y = GUNNER_AXIS_Y;
 	}
 	break;
 
 	case ZW_GAME_GUNNER_UP:
 	{
 		APP_DBG_SIG("ZW_GAME_GUNNER_UP\n");
-		if (gunner_y <= GUNNER_AXIS_Y_MIN + GUNNER_STEP_AXIS_Y)
-			gunner_y = GUNNER_AXIS_Y_MIN;
+		if (gunner.y <= GUNNER_AXIS_Y_MIN + GUNNER_STEP_AXIS_Y)
+			gunner.y = GUNNER_AXIS_Y_MIN;
 		else
-			gunner_y -= GUNNER_STEP_AXIS_Y;
+			gunner.y -= GUNNER_STEP_AXIS_Y;
 	}
 	break;
 
 	case ZW_GAME_GUNNER_DOWN:
 	{
 		APP_DBG_SIG("ZW_GAME_GUNNER_DOWN\n");
-		gunner_y += GUNNER_STEP_AXIS_Y;
-		if (gunner_y > GUNNER_AXIS_Y_MAX)
+		gunner.y += GUNNER_STEP_AXIS_Y;
+		if (gunner.y > GUNNER_AXIS_Y_MAX)
 		{
-			gunner_y = GUNNER_AXIS_Y_MAX;
+			gunner.y = GUNNER_AXIS_Y_MAX;
 		}
 	}
 	break;
 
 	case ZW_GAME_GUNNER_UPDATE:
 	{
-		gunner.y = gunner_y;
 		if (gunner.action_image == 2)
 		{
 			gunner.action_image = 1;
@@ -56,7 +53,6 @@ void zw_game_gunner_handle(ak_msg_t* msg)
 		gunner.x = GUNNER_AXIS_X;
 		gunner.y = GUNNER_AXIS_Y;
 		gunner.visible = BLACK;
-		gunner_y = GUNNER_AXIS_Y;
 	}
 	break;
 
