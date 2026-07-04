fb44c6bab14ca1d4b43677d51d670d0190d69616
Cao Trong Phuoc <caotrongphuoc@gmail.com>
2026-07-05 00:22:08 +0700

    [MOVE] define and struct into headers

--- application/sources/app/mini_clock/mc_clock_clock.h
+++ application/sources/app/mini_clock/mc_clock_clock.h
@@ -4,6 +4,17 @@
 #include "ak.h"
 #include "rtc.h"
 
+#include "app.h"
+#include "message.h"
+#include "task_list.h"
+
+typedef struct
+{
+	rtc_time_t time;
+	rtc_date_t date;
+	uint8_t is_24h_format;
+} mc_clock_clock_state_t;
+
 void mc_clock_clock_handle(ak_msg_t* msg);
 
 void mc_clock_clock_get_time(rtc_time_t* time);
