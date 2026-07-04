579b1a91e65b1c1be2b9eaa54f9a0ece5c55cff8
Cao Trong Phuoc <caotrongphuoc@gmail.com>
2026-07-04 22:54:34 +0700

    [UPDATE] Add mini_clock module boundary

--- application/sources/app/Makefile.mk
+++ application/sources/app/Makefile.mk
@@ -29,3 +29,4 @@ SOURCES_CPP += sources/app/task_if.cpp
 SOURCES_CPP += sources/app/task_rf24_if.cpp
 SOURCES_CPP += sources/app/task_uart_if.cpp
 SOURCES_CPP += sources/app/task_dbg.cpp
+SOURCES_CPP += sources/app/task_mc_rtc.cpp
