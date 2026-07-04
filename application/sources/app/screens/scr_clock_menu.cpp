3561c438e0fc74d0b929291156b920846f59092e
Cao Trong Phuoc <caotrongphuoc@gmail.com>
2026-07-05 01:28:59 +0700

    [UPDATE] Add bitmap-based clock menu layout

--- application/sources/app/screens/scr_clock_menu.cpp
+++ application/sources/app/screens/scr_clock_menu.cpp
@@ -2,10 +2,41 @@
 
 static uint8_t s_scr_clock_menu_focus = SCR_CLOCK_MENU_CLOCK;
 
+static const uint8_t s_scr_clock_menu_bitmap_clock[SCR_CLOCK_MENU_ICON_BYTES] = {
+    0,
+};
+
+static const uint8_t s_scr_clock_menu_bitmap_alarm[SCR_CLOCK_MENU_ICON_BYTES] = {
+    0,
+};
+
+static const uint8_t s_scr_clock_menu_bitmap_stopwatch[SCR_CLOCK_MENU_ICON_BYTES] = {
+    0,
+};
+
+static const uint8_t s_scr_clock_menu_bitmap_timer[SCR_CLOCK_MENU_ICON_BYTES] = {
+    0,
+};
+
+static const uint8_t s_scr_clock_menu_bitmap_setting[SCR_CLOCK_MENU_ICON_BYTES] = {
+    0,
+};
+
+static const uint8_t s_scr_clock_menu_bitmap_exit[SCR_CLOCK_MENU_ICON_BYTES] = {
+    0,
+};
+
+static const uint8_t* const s_scr_clock_menu_bitmaps[SCR_CLOCK_MENU_ITEM_NUMBER] = {
+    s_scr_clock_menu_bitmap_clock,
+    s_scr_clock_menu_bitmap_alarm,
+    s_scr_clock_menu_bitmap_stopwatch,
+    s_scr_clock_menu_bitmap_timer,
+    s_scr_clock_menu_bitmap_setting,
+    s_scr_clock_menu_bitmap_exit,
+};
+
 static void view_scr_clock_menu();
 static void scr_clock_menu_draw_item(uint8_t index);
-static void scr_clock_menu_draw_icon(uint8_t index, int16_t x, int16_t y);
-static const char* scr_clock_menu_item_label(uint8_t index);
 static void scr_clock_menu_focus_next();
 static void scr_clock_menu_focus_prev();
 static void scr_clock_menu_select();
@@ -27,8 +58,6 @@ view_screen_t scr_clock_menu = {
 static void view_scr_clock_menu()
 {
 	view_render.clear();
-	view_render.setTextColor(WHITE);
-	view_render.setTextSize(1);
 
 	for (uint8_t i = 0; i < SCR_CLOCK_MENU_ITEM_NUMBER; i++)
 	{
@@ -42,84 +71,18 @@ static void scr_clock_menu_draw_item(uint8_t index)
 	uint8_t row = index / SCR_CLOCK_MENU_COL_NUMBER;
 	int16_t x = col * SCR_CLOCK_MENU_CELL_W;
 	int16_t y = row * SCR_CLOCK_MENU_CELL_H;
-
-	if (index == s_scr_clock_menu_focus)
-	{
-		view_render.drawRect(x, y, SCR_CLOCK_MENU_CELL_W, SCR_CLOCK_MENU_CELL_H, WHITE);
-	}
-
-	scr_clock_menu_draw_icon(index, x + 21, y + SCR_CLOCK_MENU_ICON_Y_OFFSET + 7);
-	view_render.setCursor(x + 3, y + SCR_CLOCK_MENU_LABEL_Y_OFFSET);
-	view_render.print(scr_clock_menu_item_label(index));
-}
-
-static void scr_clock_menu_draw_icon(uint8_t index, int16_t x, int16_t y)
-{
-	switch (index)
-	{
-	case SCR_CLOCK_MENU_CLOCK:
-		view_render.drawCircle(x, y, 7, WHITE);
-		view_render.drawLine(x, y, x, y - 5, WHITE);
-		view_render.drawLine(x, y, x + 4, y + 2, WHITE);
-		break;
-
-	case SCR_CLOCK_MENU_ALARM:
-		view_render.drawCircle(x, y + 1, 6, WHITE);
-		view_render.drawLine(x - 6, y - 6, x - 3, y - 9, WHITE);
-		view_render.drawLine(x + 6, y - 6, x + 3, y - 9, WHITE);
-		view_render.drawLine(x - 4, y + 8, x - 7, y + 11, WHITE);
-		view_render.drawLine(x + 4, y + 8, x + 7, y + 11, WHITE);
-		break;
-
-	case SCR_CLOCK_MENU_STOPWATCH:
-		view_render.drawCircle(x, y + 1, 6, WHITE);
-		view_render.drawRect(x - 2, y - 9, 5, 3, WHITE);
-		view_render.drawLine(x, y + 1, x + 3, y - 3, WHITE);
-		break;
-
-	case SCR_CLOCK_MENU_TIMER:
-		view_render.drawRect(x - 7, y - 5, 14, 12, WHITE);
-		view_render.drawLine(x - 4, y - 8, x + 4, y - 8, WHITE);
-		view_render.drawLine(x, y - 8, x, y - 5, WHITE);
-		view_render.drawLine(x - 3, y + 1, x + 3, y + 1, WHITE);
-		break;
-
-	case SCR_CLOCK_MENU_SETTING:
-		view_render.drawCircle(x, y, 7, WHITE);
-		view_render.drawCircle(x, y, 2, WHITE);
-		view_render.drawLine(x - 10, y, x - 7, y, WHITE);
-		view_render.drawLine(x + 7, y, x + 10, y, WHITE);
-		view_render.drawLine(x, y - 10, x, y - 7, WHITE);
-		view_render.drawLine(x, y + 7, x, y + 10, WHITE);
-		break;
-
-	case SCR_CLOCK_MENU_EXIT:
-	default:
-		view_render.drawRect(x - 7, y - 7, 12, 14, WHITE);
-		view_render.drawLine(x - 2, y, x + 8, y, WHITE);
-		view_render.drawLine(x + 5, y - 3, x + 8, y, WHITE);
-		view_render.drawLine(x + 5, y + 3, x + 8, y, WHITE);
-		break;
-	}
-}
-
-static const char* scr_clock_menu_item_label(uint8_t index)
-{
-	static const char* labels[] = {
-	    "CLOCK",
-	    "ALARM",
-	    "SWATCH",
-	    "TIMER",
-	    "SET",
-	    "EXIT",
-	};
+	uint16_t bg_color = (index == s_scr_clock_menu_focus) ? WHITE : BLACK;
+	uint16_t icon_color = (index == s_scr_clock_menu_focus) ? BLACK : WHITE;
+	int16_t icon_x = x + ((SCR_CLOCK_MENU_CELL_W - SCR_CLOCK_MENU_ICON_W) / 2);
+	int16_t icon_y = y + ((SCR_CLOCK_MENU_CELL_H - SCR_CLOCK_MENU_ICON_H) / 2);
 
 	if (index >= SCR_CLOCK_MENU_ITEM_NUMBER)
 	{
-		return "";
+		return;
 	}
 
-	return labels[index];
+	view_render.fillRect(x, y, SCR_CLOCK_MENU_CELL_W, SCR_CLOCK_MENU_CELL_H, bg_color);
+	view_render.drawBitmap(icon_x, icon_y, s_scr_clock_menu_bitmaps[index], SCR_CLOCK_MENU_ICON_W, SCR_CLOCK_MENU_ICON_H, icon_color);
 }
 
 static void scr_clock_menu_focus_next()
