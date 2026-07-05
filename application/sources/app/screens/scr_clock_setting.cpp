f45e6baf2edb14b7bf594c6c69ec0306fcb554a9
Cao Trong Phuoc <caotrongphuoc@gmail.com>
2026-07-05 11:11:53 +0700

    [UPDATE] Reorder clock setting screen code

--- application/sources/app/screens/scr_clock_setting.cpp
+++ application/sources/app/screens/scr_clock_setting.cpp
@@ -1,5 +1,9 @@
 #include "scr_clock_setting.h"
 
+/*****************************************************************************/
+/* Variable Declaration - Clock setting */
+/*****************************************************************************/
+
 rtc_time_t scr_clock_setting_time;
 rtc_date_t scr_clock_setting_date;
 uint8_t scr_clock_setting_field = SCR_CLOCK_SETTING_YEAR;
@@ -14,83 +18,70 @@ const char* const scr_clock_setting_field_name[SCR_CLOCK_SETTING_FIELD_NUMBER] =
     "SAVE",
 };
 
-void view_scr_clock_setting();
-void scr_clock_setting_load();
-void scr_clock_setting_change_value(int8_t step);
-void scr_clock_setting_next_field();
-void scr_clock_setting_save();
-void scr_clock_setting_fix_date();
-void scr_clock_setting_write_2_digit(char* buffer, uint8_t value);
-void scr_clock_setting_write_4_digit(char* buffer, uint16_t value);
-uint8_t scr_clock_setting_days_in_month(uint16_t year, uint8_t month);
-uint8_t scr_clock_setting_weekday(uint16_t year, uint8_t month, uint8_t date);
-uint16_t scr_clock_setting_wrap_value(uint16_t value, uint16_t min, uint16_t max, int8_t step);
+/*****************************************************************************/
+/* Value helpers - Clock setting */
+/*****************************************************************************/
 
-view_dynamic_t dyn_view_scr_clock_setting = {
-    {
-        .item_type = ITEM_TYPE_DYNAMIC,
-    },
-    view_scr_clock_setting};
-
-view_screen_t scr_clock_setting = {
-    &dyn_view_scr_clock_setting,
-    ITEM_NULL,
-    ITEM_NULL,
-
-    .focus_item = 0,
-};
-
-void view_scr_clock_setting()
+uint16_t scr_clock_setting_wrap_value(uint16_t value, uint16_t min, uint16_t max, int8_t step)
 {
-	char date_text[11];
-	char time_text[9];
-
-	scr_clock_setting_write_4_digit(&date_text[0], scr_clock_setting_date.year);
-	date_text[4] = '-';
-	scr_clock_setting_write_2_digit(&date_text[5], scr_clock_setting_date.month);
-	date_text[7] = '-';
-	scr_clock_setting_write_2_digit(&date_text[8], scr_clock_setting_date.date);
-	date_text[10] = '\0';
-
-	scr_clock_setting_write_2_digit(&time_text[0], scr_clock_setting_time.hour);
-	time_text[2] = ':';
-	scr_clock_setting_write_2_digit(&time_text[3], scr_clock_setting_time.min);
-	time_text[5] = ':';
-	scr_clock_setting_write_2_digit(&time_text[6], scr_clock_setting_time.sec);
-	time_text[8] = '\0';
-
-	view_render.clear();
-	view_render.setTextSize(1);
-	view_render.setTextColor(WHITE);
-
-	view_render.setCursor(0, 0);
-	view_render.print("SET ");
-	view_render.print(scr_clock_setting_field_name[scr_clock_setting_field]);
-
-	view_render.setCursor(16, 18);
-	view_render.print(date_text);
-
-	view_render.setCursor(16, 32);
-	view_render.print(time_text);
-
-	if (scr_clock_setting_field == SCR_CLOCK_SETTING_SAVE)
+	if (step > 0)
 	{
-		view_render.fillRect(84, 50, 36, 10, WHITE);
-		view_render.setTextColor(BLACK);
-		view_render.setCursor(90, 51);
-		view_render.print("SAVE");
-		view_render.setTextColor(WHITE);
+		return (value >= max) ? min : value + 1;
+	}
+
+	return (value <= min) ? max : value - 1;
+}
+
+uint8_t scr_clock_setting_days_in_month(uint16_t year, uint8_t month)
+{
+	const uint8_t days[] = {
+	    31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+	if (month == 0 || month > 12)
+	{
+		return 31;
+	}
+
+	if (month == 2 && ((year % 4) == 0))
+	{
+		return 29;
+	}
+
+	return days[month - 1];
+}
+
+void scr_clock_setting_fix_date()
+{
+	uint8_t max_date = scr_clock_setting_days_in_month(scr_clock_setting_date.year,
+	                                                   scr_clock_setting_date.month);
+
+	if (scr_clock_setting_date.date > max_date)
+	{
+		scr_clock_setting_date.date = max_date;
 	}
 }
 
-void scr_clock_setting_load()
+uint8_t scr_clock_setting_weekday(uint16_t year, uint8_t month, uint8_t date)
 {
-	mc_clock_clock_state_t clock_state;
+	if (month < 3)
+	{
+		month += 12;
+		year--;
+	}
 
-	mc_clock_clock_get_state(&clock_state);
-	scr_clock_setting_time = clock_state.time;
-	scr_clock_setting_date = clock_state.date;
-	scr_clock_setting_fix_date();
+	uint16_t weekday = (date + ((13 * (month + 1)) / 5) + year + (year / 4) - (year / 100) + (year / 400)) % 7;
+
+	if (weekday == 0)
+	{
+		return RTC_WEEKDAY_SUN - 1;
+	}
+
+	if (weekday == 1)
+	{
+		return RTC_WEEKDAY_SUN;
+	}
+
+	return weekday - 1;
 }
 
 void scr_clock_setting_change_value(int8_t step)
@@ -136,39 +127,9 @@ void scr_clock_setting_change_value(int8_t step)
 	}
 }
 
-void scr_clock_setting_next_field()
-{
-	if (scr_clock_setting_field == SCR_CLOCK_SETTING_SAVE)
-	{
-		scr_clock_setting_save();
-		SCREEN_BACK();
-		return;
-	}
-
-	scr_clock_setting_field++;
-}
-
-void scr_clock_setting_save()
-{
-	scr_clock_setting_date.weekday = scr_clock_setting_weekday(scr_clock_setting_date.year,
-	                                                           scr_clock_setting_date.month,
-	                                                           scr_clock_setting_date.date);
-
-	rtc_set_date(&scr_clock_setting_date);
-	rtc_set_time(&scr_clock_setting_time);
-	task_post_pure_msg(MC_CLOCK_CLOCK_ID, MC_CLOCK_CLOCK_TICK);
-}
-
-void scr_clock_setting_fix_date()
-{
-	uint8_t max_date = scr_clock_setting_days_in_month(scr_clock_setting_date.year,
-	                                                   scr_clock_setting_date.month);
-
-	if (scr_clock_setting_date.date > max_date)
-	{
-		scr_clock_setting_date.date = max_date;
-	}
-}
+/*****************************************************************************/
+/* View - Clock setting */
+/*****************************************************************************/
 
 void scr_clock_setting_write_2_digit(char* buffer, uint8_t value)
 {
@@ -184,56 +145,66 @@ void scr_clock_setting_write_4_digit(char* buffer, uint16_t value)
 	buffer[3] = (value % 10) + '0';
 }
 
-uint8_t scr_clock_setting_days_in_month(uint16_t year, uint8_t month)
+void view_scr_clock_setting()
 {
-	const uint8_t days[] = {
-	    31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	char date_text[11];
+	char time_text[9];
 
-	if (month == 0 || month > 12)
+	scr_clock_setting_write_4_digit(&date_text[0], scr_clock_setting_date.year);
+	date_text[4] = '-';
+	scr_clock_setting_write_2_digit(&date_text[5], scr_clock_setting_date.month);
+	date_text[7] = '-';
+	scr_clock_setting_write_2_digit(&date_text[8], scr_clock_setting_date.date);
+	date_text[10] = '\0';
+
+	scr_clock_setting_write_2_digit(&time_text[0], scr_clock_setting_time.hour);
+	time_text[2] = ':';
+	scr_clock_setting_write_2_digit(&time_text[3], scr_clock_setting_time.min);
+	time_text[5] = ':';
+	scr_clock_setting_write_2_digit(&time_text[6], scr_clock_setting_time.sec);
+	time_text[8] = '\0';
+
+	view_render.clear();
+	view_render.setTextSize(1);
+	view_render.setTextColor(WHITE);
+
+	view_render.setCursor(0, 0);
+	view_render.print("SET ");
+	view_render.print(scr_clock_setting_field_name[scr_clock_setting_field]);
+
+	view_render.setCursor(16, 18);
+	view_render.print(date_text);
+
+	view_render.setCursor(16, 32);
+	view_render.print(time_text);
+
+	if (scr_clock_setting_field == SCR_CLOCK_SETTING_SAVE)
 	{
-		return 31;
+		view_render.fillRect(84, 50, 36, 10, WHITE);
+		view_render.setTextColor(BLACK);
+		view_render.setCursor(90, 51);
+		view_render.print("SAVE");
+		view_render.setTextColor(WHITE);
 	}
-
-	if (month == 2 && ((year % 4) == 0))
-	{
-		return 29;
-	}
-
-	return days[month - 1];
 }
 
-uint8_t scr_clock_setting_weekday(uint16_t year, uint8_t month, uint8_t date)
-{
-	if (month < 3)
-	{
-		month += 12;
-		year--;
-	}
+view_dynamic_t dyn_view_scr_clock_setting = {
+    {
+        .item_type = ITEM_TYPE_DYNAMIC,
+    },
+    view_scr_clock_setting};
 
-	uint16_t weekday = (date + ((13 * (month + 1)) / 5) + year + (year / 4) - (year / 100) + (year / 400)) % 7;
+view_screen_t scr_clock_setting = {
+    &dyn_view_scr_clock_setting,
+    ITEM_NULL,
+    ITEM_NULL,
 
-	if (weekday == 0)
-	{
-		return RTC_WEEKDAY_SUN - 1;
-	}
+    .focus_item = 0,
+};
 
-	if (weekday == 1)
-	{
-		return RTC_WEEKDAY_SUN;
-	}
-
-	return weekday - 1;
-}
-
-uint16_t scr_clock_setting_wrap_value(uint16_t value, uint16_t min, uint16_t max, int8_t step)
-{
-	if (step > 0)
-	{
-		return (value >= max) ? min : value + 1;
-	}
-
-	return (value <= min) ? max : value - 1;
-}
+/*****************************************************************************/
+/* Handle - Clock setting */
+/*****************************************************************************/
 
 void scr_clock_setting_handle(ak_msg_t* msg)
 {
@@ -242,15 +213,33 @@ void scr_clock_setting_handle(ak_msg_t* msg)
 	case SCREEN_ENTRY:
 	{
 		APP_DBG_SIG("SCREEN_ENTRY\n");
+		mc_clock_clock_state_t clock_state;
+
+		mc_clock_clock_get_state(&clock_state);
+		scr_clock_setting_time = clock_state.time;
+		scr_clock_setting_date = clock_state.date;
 		scr_clock_setting_field = SCR_CLOCK_SETTING_YEAR;
-		scr_clock_setting_load();
+		scr_clock_setting_fix_date();
 	}
 	break;
 
 	case AC_DISPLAY_BUTON_MODE_PRESSED:
 	{
 		APP_DBG_SIG("AC_DISPLAY_BUTON_MODE_PRESSED\n");
-		scr_clock_setting_next_field();
+		if (scr_clock_setting_field == SCR_CLOCK_SETTING_SAVE)
+		{
+			scr_clock_setting_date.weekday = scr_clock_setting_weekday(scr_clock_setting_date.year,
+			                                                           scr_clock_setting_date.month,
+			                                                           scr_clock_setting_date.date);
+			rtc_set_date(&scr_clock_setting_date);
+			rtc_set_time(&scr_clock_setting_time);
+			task_post_pure_msg(MC_CLOCK_CLOCK_ID, MC_CLOCK_CLOCK_TICK);
+			SCREEN_BACK();
+		}
+		else
+		{
+			scr_clock_setting_field++;
+		}
 		task_post_pure_msg(AC_TASK_DISPLAY_ID, AC_DISPLAY_RENDER_SCREEN);
 	}
 	break;
