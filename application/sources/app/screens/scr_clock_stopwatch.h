8d290e40ecaacf9e747f91fd57530c61e035f1c5
Cao Trong Phuoc <caotrongphuoc@gmail.com>
2026-07-09 09:00:46 +0700

    [FIX] Trigger clock face setup on SCREEN_ENTRY instead of AC_DISPLAY_INITIAL

--- application/sources/app/screens/scr_clock_stopwatch.h
+++ application/sources/app/screens/scr_clock_stopwatch.h
@@ -2,25 +2,5 @@
 #define __SCR_CLOCK_STOPWATCH_H__
 
 #include "screens.h"
-#include "ak.h"
-#include "app_dbg.h"
-#include "task_list.h"
-#include "message.h"
-#include <stdio.h>
-
-typedef enum
-{
-    SCR_CLOCK_STOPWATCH_DISPLAY,
-    SCR_CLOCK_STOPWATCH_START,
-    SCR_CLOCK_STOPWATCH_PAUSE,
-    SCR_CLOCK_STOPWATCH_RESET,
-    SCR_CLOCK_STOPWATCH_LAP,
-    SCR_CLOCK_STOPWATCH_BACK,
-} scr_clock_stopwatch_item_t;
-
-
-/* Called every 10ms */
-void stopwatch_background_update(void);
-
 
 #endif // __SCR_CLOCK_STOPWATCH_H__
