b5bfe171d1955adbfcff2adc4306165f808da145
Cao Trong Phuoc <caotrongphuoc@gmail.com>
2026-07-09 11:27:11 +0700

    [FEAT] Route clock menu exit to idle screen

--- application/sources/app/screens/scr_clock_menu.cpp
+++ application/sources/app/screens/scr_clock_menu.cpp
@@ -96,7 +96,7 @@ void scr_clock_menu_handle(ak_msg_t* msg)
             break;
 
         case SCR_CLOCK_MENU_EXIT:
-            SCREEN_TRAN(scr_clock_main_handle, &scr_clock_main);
+            SCREEN_TRAN(scr_idle_handle, &scr_idle);
             break;
 
         default:
