9bbba483e43c4d2a334ad0410b55de2f4ce97871
Cao Trong Phuoc <caotrongphuoc@gmail.com>
2026-07-09 12:07:48 +0700

    [UI] Use seven segment clock face

--- application/sources/app/screens/scr_clock_main.cpp
+++ application/sources/app/screens/scr_clock_main.cpp
@@ -17,20 +17,6 @@ const char* scr_clock_main_weekday_text(uint8_t weekday)
 	return weekday_text[weekday];
 }
 
-const char* scr_clock_main_month_text(uint8_t month)
-{
-	const char* month_text[] = {
-	    "---", "JAN", "FEB", "MAR", "APR", "MAY", "JUN",
-	    "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"};
-
-	if (month == 0 || month > 12)
-	{
-		return month_text[0];
-	}
-
-	return month_text[month];
-}
-
 void scr_clock_main_write_2_digit(char* buffer, uint8_t value)
 {
 	buffer[0] = (value / 10) + '0';
@@ -65,123 +51,119 @@ void scr_clock_main_format_date(char* buffer, rtc_date_t* date)
 	buffer[10] = '\0';
 }
 
-uint8_t scr_clock_main_days_in_month(uint16_t year, uint8_t month)
+void scr_clock_main_draw_horizontal_segment(int16_t x, int16_t y, uint8_t color)
 {
-	const uint8_t days[] = {
-	    31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-
-	if (month == 0 || month > 12)
-	{
-		return 31;
-	}
-
-	if (month == 2 && ((year % 4) == 0))
-	{
-		return 29;
-	}
-
-	return days[month - 1];
+	view_render.fillRect(x + SCR_CLOCK_MAIN_SEG_T,
+	                     y,
+	                     SCR_CLOCK_MAIN_SEG_W - (2 * SCR_CLOCK_MAIN_SEG_T),
+	                     SCR_CLOCK_MAIN_SEG_T,
+	                     color);
 }
 
-uint8_t scr_clock_main_weekday(uint16_t year, uint8_t month, uint8_t date)
+void scr_clock_main_draw_vertical_segment(int16_t x, int16_t y, uint8_t color)
 {
-	if (month < 3)
-	{
-		month += 12;
-		year--;
-	}
-
-	uint16_t year_in_century = year % 100;
-	uint16_t century = year / 100;
-	uint16_t weekday = (date + ((13 * (month + 1)) / 5) +
-	                    year_in_century + (year_in_century / 4) +
-	                    (century / 4) + (5 * century)) % 7;
-
-	if (weekday == 0)
-	{
-		return RTC_WEEKDAY_SUN - 1;
-	}
-
-	if (weekday == 1)
-	{
-		return RTC_WEEKDAY_SUN;
-	}
-
-	return weekday - 1;
+	view_render.fillRect(x,
+	                     y,
+	                     SCR_CLOCK_MAIN_SEG_T,
+	                     (SCR_CLOCK_MAIN_SEG_H / 2) - SCR_CLOCK_MAIN_SEG_T,
+	                     color);
 }
 
-void scr_clock_main_format_hour_min(char* buffer, rtc_time_t* time)
+void scr_clock_main_draw_digit(uint8_t digit, int16_t x, int16_t y)
 {
-	scr_clock_main_write_2_digit(&buffer[0], time->hour);
-	buffer[2] = ':';
-	scr_clock_main_write_2_digit(&buffer[3], time->min);
-	buffer[5] = '\0';
+	const uint8_t digit_mask[] = {
+	    0x3F, 0x06, 0x5B, 0x4F, 0x66,
+	    0x6D, 0x7D, 0x07, 0x7F, 0x6F};
+	uint8_t mask = digit_mask[digit % 10];
+	int16_t middle_y = y + (SCR_CLOCK_MAIN_SEG_H / 2) - (SCR_CLOCK_MAIN_SEG_T / 2);
+	int16_t bottom_y = y + SCR_CLOCK_MAIN_SEG_H - SCR_CLOCK_MAIN_SEG_T;
+	int16_t right_x = x + SCR_CLOCK_MAIN_SEG_W - SCR_CLOCK_MAIN_SEG_T;
+	int16_t lower_y = y + (SCR_CLOCK_MAIN_SEG_H / 2);
+
+	if (mask & 0x01)
+	{
+		scr_clock_main_draw_horizontal_segment(x, y, WHITE);
+	}
+	if (mask & 0x02)
+	{
+		scr_clock_main_draw_vertical_segment(right_x, y + SCR_CLOCK_MAIN_SEG_T, WHITE);
+	}
+	if (mask & 0x04)
+	{
+		scr_clock_main_draw_vertical_segment(right_x, lower_y, WHITE);
+	}
+	if (mask & 0x08)
+	{
+		scr_clock_main_draw_horizontal_segment(x, bottom_y, WHITE);
+	}
+	if (mask & 0x10)
+	{
+		scr_clock_main_draw_vertical_segment(x, lower_y, WHITE);
+	}
+	if (mask & 0x20)
+	{
+		scr_clock_main_draw_vertical_segment(x, y + SCR_CLOCK_MAIN_SEG_T, WHITE);
+	}
+	if (mask & 0x40)
+	{
+		scr_clock_main_draw_horizontal_segment(x, middle_y, WHITE);
+	}
 }
 
-void scr_clock_main_print_day(uint8_t day, uint8_t col, uint8_t row, uint8_t selected)
+void scr_clock_main_draw_colon(int16_t x, int16_t y)
 {
-	char day_text[3];
-	int16_t x = SCR_CLOCK_MAIN_CAL_X + (col * SCR_CLOCK_MAIN_CAL_COL_STEP);
-	int16_t y = SCR_CLOCK_MAIN_CAL_DAY_Y + (row * SCR_CLOCK_MAIN_CAL_ROW_STEP);
-
-	if (selected)
-	{
-		view_render.fillRect(x - 1, y - 1, 13, 8, WHITE);
-		view_render.setTextColor(BLACK);
-	}
-	else
-	{
-		view_render.setTextColor(WHITE);
-	}
-
-	if (day >= 10)
-	{
-		scr_clock_main_write_2_digit(day_text, day);
-		day_text[2] = '\0';
-	}
-	else
-	{
-		day_text[0] = '0' + day;
-		day_text[1] = '\0';
-	}
-
-	view_render.setCursor(x, y);
-	view_render.print(day_text);
-	view_render.setTextColor(WHITE);
+	view_render.fillRect(x, y + 7, 3, 3, WHITE);
+	view_render.fillRect(x, y + 16, 3, 3, WHITE);
 }
 
-void scr_clock_main_draw_calendar(rtc_date_t* date)
+void scr_clock_main_draw_time(rtc_time_t* time)
 {
-	const char* weekdays = "MTWTFSS";
-	uint8_t first_weekday = scr_clock_main_weekday(date->year, date->month, 1);
-	uint8_t days = scr_clock_main_days_in_month(date->year, date->month);
-	uint8_t start_col = first_weekday - RTC_WEEKDAY_MON;
+	int16_t x = SCR_CLOCK_MAIN_SEG_X;
+	int16_t y = SCR_CLOCK_MAIN_SEG_Y;
 
+	scr_clock_main_draw_digit(time->hour / 10, x, y);
+	x += 15;
+	scr_clock_main_draw_digit(time->hour % 10, x, y);
+	x += 16;
+	scr_clock_main_draw_colon(x, y);
+	x += 7;
+
+	scr_clock_main_draw_digit(time->min / 10, x, y);
+	x += 15;
+	scr_clock_main_draw_digit(time->min % 10, x, y);
+	x += 16;
+	scr_clock_main_draw_colon(x, y);
+	x += 7;
+
+	scr_clock_main_draw_digit(time->sec / 10, x, y);
+	x += 15;
+	scr_clock_main_draw_digit(time->sec % 10, x, y);
+}
+
+void scr_clock_main_draw_weekdays(uint8_t weekday)
+{
 	view_render.setTextSize(1);
-	view_render.setTextColor(WHITE);
-	view_render.setCursor(SCR_CLOCK_MAIN_CAL_X + 20, SCR_CLOCK_MAIN_CAL_MONTH_Y);
-	view_render.print(scr_clock_main_month_text(date->month));
 
-	for (uint8_t col = 0; col < 7; col++)
+	for (uint8_t i = RTC_WEEKDAY_MON; i <= RTC_WEEKDAY_SUN; i++)
 	{
-		view_render.setCursor(SCR_CLOCK_MAIN_CAL_X + (col * SCR_CLOCK_MAIN_CAL_COL_STEP),
-		                      SCR_CLOCK_MAIN_CAL_WEEKDAY_Y);
-		view_render.print(weekdays[col]);
-	}
+		int16_t x = SCR_CLOCK_MAIN_WEEKDAY_ROW_X + ((i - RTC_WEEKDAY_MON) * 18);
+		uint8_t selected = (i == weekday);
 
-	for (uint8_t day = 1; day <= days; day++)
-	{
-		uint8_t index = start_col + day - 1;
-		uint8_t col = index % 7;
-		uint8_t row = index / 7;
-
-		if (row > 5)
+		if (selected)
 		{
-			break;
+			view_render.fillRect(x - 1, SCR_CLOCK_MAIN_WEEKDAY_ROW_Y - 1, 18, 9, WHITE);
+			view_render.setTextColor(BLACK);
+		}
+		else
+		{
+			view_render.setTextColor(WHITE);
 		}
 
-		scr_clock_main_print_day(day, col, row, day == date->date);
+		view_render.setCursor(x, SCR_CLOCK_MAIN_WEEKDAY_ROW_Y);
+		view_render.print(scr_clock_main_weekday_text(i));
 	}
+
+	view_render.setTextColor(WHITE);
 }
 
 /*****************************************************************************/
@@ -207,26 +189,20 @@ view_screen_t scr_clock_main = {
 void view_scr_clock_main()
 {
 	mc_clock_time_state_t clock_state;
-	char time_text[6];
-	char sec_text[3];
+	char date_text[11];
 
 	mc_clock_time_get_state(&clock_state);
-	scr_clock_main_format_hour_min(time_text, &clock_state.time);
-	scr_clock_main_write_2_digit(sec_text, clock_state.time.sec);
-	sec_text[2] = '\0';
+	scr_clock_main_format_date(date_text, &clock_state.date);
 
 	view_render.clear();
 	view_render.setTextColor(WHITE);
 
-	view_render.setTextSize(SCR_CLOCK_MAIN_TIME_TEXT_SIZE);
-	view_render.setCursor(SCR_CLOCK_MAIN_TIME_X, SCR_CLOCK_MAIN_TIME_Y);
-	view_render.print(time_text);
+	scr_clock_main_draw_weekdays(clock_state.date.weekday);
+	scr_clock_main_draw_time(&clock_state.time);
 
 	view_render.setTextSize(SCR_CLOCK_MAIN_INFO_TEXT_SIZE);
-	view_render.setCursor(SCR_CLOCK_MAIN_SEC_X, SCR_CLOCK_MAIN_SEC_Y);
-	view_render.print(sec_text);
-
-	scr_clock_main_draw_calendar(&clock_state.date);
+	view_render.setCursor(SCR_CLOCK_MAIN_DATE_TEXT_X, SCR_CLOCK_MAIN_DATE_TEXT_Y);
+	view_render.print(date_text);
 }
 
 /*****************************************************************************/
