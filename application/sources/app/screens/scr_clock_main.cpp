92397354eae88c325fc7d4566b7fc51527642d23
Cao Trong Phuoc <caotrongphuoc@gmail.com>
2026-07-09 11:59:20 +0700

    [UI] Simplify digital calendar clock face

--- application/sources/app/screens/scr_clock_main.cpp
+++ application/sources/app/screens/scr_clock_main.cpp
@@ -118,93 +118,36 @@ void scr_clock_main_format_hour_min(char* buffer, rtc_time_t* time)
 	buffer[5] = '\0';
 }
 
-void scr_clock_main_draw_second_tick(uint8_t index, uint8_t second)
-{
-	int16_t x;
-	int16_t y;
-	uint8_t offset = index % 15;
-
-	if (index < 15)
-	{
-		x = SCR_CLOCK_MAIN_FRAME_X + 4 + (offset * 4);
-		y = SCR_CLOCK_MAIN_FRAME_Y;
-		view_render.drawLine(x, y, x, y + (index == second ? 4 : 2), WHITE);
-	}
-	else if (index < 30)
-	{
-		x = SCR_CLOCK_MAIN_FRAME_X + SCR_CLOCK_MAIN_FRAME_W - 1;
-		y = SCR_CLOCK_MAIN_FRAME_Y + 3 + (offset * 3);
-		view_render.drawLine(x, y, x - (index == second ? 4 : 2), y, WHITE);
-	}
-	else if (index < 45)
-	{
-		x = SCR_CLOCK_MAIN_FRAME_X + SCR_CLOCK_MAIN_FRAME_W - 5 - (offset * 4);
-		y = SCR_CLOCK_MAIN_FRAME_Y + SCR_CLOCK_MAIN_FRAME_H - 1;
-		view_render.drawLine(x, y, x, y - (index == second ? 4 : 2), WHITE);
-	}
-	else
-	{
-		x = SCR_CLOCK_MAIN_FRAME_X;
-		y = SCR_CLOCK_MAIN_FRAME_Y + SCR_CLOCK_MAIN_FRAME_H - 4 - (offset * 3);
-		view_render.drawLine(x, y, x + (index == second ? 4 : 2), y, WHITE);
-	}
-}
-
-void scr_clock_main_draw_second_frame(uint8_t second)
-{
-	for (uint8_t i = 0; i < 60; i++)
-	{
-		scr_clock_main_draw_second_tick(i, second);
-	}
-}
-
-void scr_clock_main_draw_calendar_digit(uint8_t digit, int16_t x, int16_t y, uint8_t color)
-{
-	const uint8_t digit_map[10][5] = {
-	    {0x07, 0x05, 0x05, 0x05, 0x07},
-	    {0x02, 0x06, 0x02, 0x02, 0x07},
-	    {0x07, 0x01, 0x07, 0x04, 0x07},
-	    {0x07, 0x01, 0x07, 0x01, 0x07},
-	    {0x05, 0x05, 0x07, 0x01, 0x01},
-	    {0x07, 0x04, 0x07, 0x01, 0x07},
-	    {0x07, 0x04, 0x07, 0x05, 0x07},
-	    {0x07, 0x01, 0x01, 0x01, 0x01},
-	    {0x07, 0x05, 0x07, 0x05, 0x07},
-	    {0x07, 0x05, 0x07, 0x01, 0x07},
-	};
-
-	for (uint8_t row = 0; row < 5; row++)
-	{
-		for (uint8_t col = 0; col < 3; col++)
-		{
-			if (digit_map[digit][row] & (1 << (2 - col)))
-			{
-				view_render.drawPixel(x + col, y + row, color);
-			}
-		}
-	}
-}
-
 void scr_clock_main_print_day(uint8_t day, uint8_t col, uint8_t row, uint8_t selected)
 {
+	char day_text[3];
 	int16_t x = SCR_CLOCK_MAIN_CAL_X + (col * SCR_CLOCK_MAIN_CAL_COL_STEP);
 	int16_t y = SCR_CLOCK_MAIN_CAL_DAY_Y + (row * SCR_CLOCK_MAIN_CAL_ROW_STEP);
-	uint8_t color = selected ? BLACK : WHITE;
 
 	if (selected)
 	{
-		view_render.fillRect(x - 1, y - 1, 8, 7, WHITE);
+		view_render.fillRect(x - 1, y - 1, 13, 8, WHITE);
+		view_render.setTextColor(BLACK);
+	}
+	else
+	{
+		view_render.setTextColor(WHITE);
 	}
 
 	if (day >= 10)
 	{
-		scr_clock_main_draw_calendar_digit(day / 10, x, y, color);
-		scr_clock_main_draw_calendar_digit(day % 10, x + 4, y, color);
+		scr_clock_main_write_2_digit(day_text, day);
+		day_text[2] = '\0';
 	}
 	else
 	{
-		scr_clock_main_draw_calendar_digit(day, x + 2, y, color);
+		day_text[0] = '0' + day;
+		day_text[1] = '\0';
 	}
+
+	view_render.setCursor(x, y);
+	view_render.print(day_text);
+	view_render.setTextColor(WHITE);
 }
 
 void scr_clock_main_draw_calendar(rtc_date_t* date)
@@ -216,7 +159,7 @@ void scr_clock_main_draw_calendar(rtc_date_t* date)
 
 	view_render.setTextSize(1);
 	view_render.setTextColor(WHITE);
-	view_render.setCursor(SCR_CLOCK_MAIN_CAL_X + 14, SCR_CLOCK_MAIN_CAL_MONTH_Y);
+	view_render.setCursor(SCR_CLOCK_MAIN_CAL_X + 20, SCR_CLOCK_MAIN_CAL_MONTH_Y);
 	view_render.print(scr_clock_main_month_text(date->month));
 
 	for (uint8_t col = 0; col < 7; col++)
@@ -265,18 +208,23 @@ void view_scr_clock_main()
 {
 	mc_clock_time_state_t clock_state;
 	char time_text[6];
+	char sec_text[3];
 
 	mc_clock_time_get_state(&clock_state);
 	scr_clock_main_format_hour_min(time_text, &clock_state.time);
+	scr_clock_main_write_2_digit(sec_text, clock_state.time.sec);
+	sec_text[2] = '\0';
 
 	view_render.clear();
 	view_render.setTextColor(WHITE);
 
-	scr_clock_main_draw_second_frame(clock_state.time.sec);
-
 	view_render.setTextSize(SCR_CLOCK_MAIN_TIME_TEXT_SIZE);
 	view_render.setCursor(SCR_CLOCK_MAIN_TIME_X, SCR_CLOCK_MAIN_TIME_Y);
 	view_render.print(time_text);
+
+	view_render.setTextSize(SCR_CLOCK_MAIN_INFO_TEXT_SIZE);
+	view_render.setCursor(SCR_CLOCK_MAIN_SEC_X, SCR_CLOCK_MAIN_SEC_Y);
+	view_render.print(sec_text);
 
 	scr_clock_main_draw_calendar(&clock_state.date);
 }
